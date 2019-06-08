ce0fd54c5f0192d1076ef1754f0f228fa48a0428
Fortaleza <fortaleza555@hotmail.com>
2019-06-08 02:32:04 -0300

    resolvnedo bug do paramentro

--- public/src/js/model/ListModel.js
+++ public/src/js/model/ListModel.js
@@ -1,88 +1,89 @@
-export default class ListModel{
-	constructor(){
+export default class ListModel {
+	constructor() {
 		this.id = "";
 		this.item = "";
 		this.qtde = "1";
 		this.name = "";
 		this.tel = "";
 	}
-	get Id(){
+	get Id() {
 		return this.id;
 	}
-	set Id(value){
-		if(String(value).length>0){
+	set Id(value) {
+		if (String(value).length > 0) {
 			this.id = value;
-		}		
+		}
 	}
-	get Item(){
+	get Item() {
 		return this.item;
 	}
-	set Item(value){
+	set Item(value) {
 		this.item = value;
 	}
-	get Qtde(){
+	get Qtde() {
 		return this.qtde;
 	}
-	set Qtde(value){
+	set Qtde(value) {
 		this.qtde = value;
 	}
-	get Name(){
+	get Name() {
 		return this.name;
 	}
-	set Name(value){
+	set Name(value) {
 		this.name = value;
 	}
-	get Tel(){
+	get Tel() {
 		return this.tel;
 	}
-	set Tel(value){
+	set Tel(value) {
 		this.tel = value;
 	}
-	returnListJson(){
-    let user = {};
-    if(this.id){ 
-      user={
-        id:this.Id,
-        item:this.Item,
-        qtde:this.Qtde,
-		    name:this.Name,
-		    tel:this.Tel
-      } 
-  }else{
-    user={
-		item:this.Item,
-    qtde:"1",
-		name:this.Name,
-		tel:this.Tel
-    }
-  }
+	returnListJson() {
+		let user = {};
+		if (this.id) {
+			user = {
+				id: this.Id,
+				item: this.Item,
+				qtde: this.Qtde,
+				name: this.Name,
+				tel: this.Tel
+			}
+		} else {
+			user = {
+				item: this.Item,
+				qtde: "1",
+				name: this.Name,
+				tel: this.Tel
+			}
+		}
 		return user;
 	}
-	loadListFromForm(jsonList){
-    if(jsonList.id){
-      this.Id = jsonList.id;
-    }
-    this.Item = jsonList.item;
-    this.Name = jsonList.name;
-    this.Tel = jsonList.tel
+	loadListFromForm(jsonList) {
+		if (jsonList.id) {
+			this.Id = jsonList.id;
+		}
+		this.Item = jsonList.item;
+		this.Name = jsonList.name;
+		this.Tel = jsonList.tel
 	}
-  insertList(listJson){
-	  this.loadListFromForm(listJson);
-    this.getReference('/list').push(this.returnListJson());
-  }
-  listItens(){
-    return new Promise((reject, resolve) =>{
-      this.getReference('/list').on('value',(item)=>{
-        if(!item.val()){
-          resolve(item.val().value);
-        }else{
-          reject(item.val());
-        }  
-      });
-        
-    })    
-  }
-  getReference(route){
-    return firebase.database().ref(route);
-  }
+	insertList(listJson) {
+		this.loadListFromForm(listJson);
+		this.getReference('/list').push(this.returnListJson());
+	}
+	listItens() {
+		return new Promise((reject, resolve) => {
+			this.getReference('/list').on('value', (item) => {
+				console.log(item.val())
+				if (item.val() != null) {
+					resolve(item);
+				} else {
+					reject(item.val());
+				}
+			});
+
+		})
+	}
+	getReference(route) {
+		return firebase.database().ref(route);
+	}
 }
