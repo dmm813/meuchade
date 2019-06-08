78e1d14276bd6c15fb662b793fe7f1051077b33f
Fortaleza <fortaleza555@hotmail.com>
2019-06-08 01:53:29 -0300

    criando modal

--- public/src/js/controller/ListController.js
+++ public/src/js/controller/ListController.js
@@ -1,27 +1,27 @@
-export default class listController{
-  constructor(list){
+export default class listController {
+  constructor(list) {
     this.listModel = list;
-    this.btnSaveEl = document.getElementById("id-reg-btnSave");
+    //this.btnSaveEl = document.getElementById("id-reg-btnSave");
     this.registerList();
-  }  
-  registerList(){
-    this.btnSaveEl.addEventListener("click", (event)=>{
-      event.preventDefault();
-      let listJson = {
-        item:'liquidificador',
-        qtde:'1',
-        name:"davidson",
-        tel:"996409225"
-      };
-      this.listModel.insertList(listJson);
-    });
+  }
+  registerList() {
+    // this.btnSaveEl.addEventListener("click", (event)=>{
+    //   event.preventDefault();
+    //   let listJson = {
+    //     item:'liquidificador',
+    //     qtde:'1',
+    //     name:"davidson",
+    //     tel:"996409225"
+    //   };
+    //   this.listModel.insertList(listJson);
+    // });
   }
 
-  loadUserJson(form){
+  loadUserJson(form) {
     var userJson = {};
     [...form].forEach(element => {
-      if (element.name.match(/txt.\w+/g)){
-        userJson[element.name.split("-")[1]] =  element.value;
+      if (element.name.match(/txt.\w+/g)) {
+        userJson[element.name.split("-")[1]] = element.value;
       }
     });
     console.log(userJson);
